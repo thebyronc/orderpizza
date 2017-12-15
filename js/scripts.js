26efa48578e59a8729b5e8380c2d81976add84fd
Byron Chang <thebyronc@gmail.com>
2017-12-15 12:14:34 -0800

    add cost to pizza

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,12 @@ function Pizza(crust, cheese, meats, veggies) {
   this.cheese = cheese;
   this.meats = meats;
   this.veggies = veggies;
-  this.cost = this.meats.length + this.veggies.length;
+  this.cost = 10 + this.meats.length + this.veggies.length;
 }
 Pizza.prototype.displayOrder = function() {
-  return "You've orderd a " + this.crust + " pizza with "+ this.cheese + "  and the following toppings: "  + this.meats + this.veggies;
+  return "You've orderd a " + this.crust + " pizza with "+ this.cheese + "  and the following toppings: "  + this.meats + " " + this.veggies + " Cost: " + this.cost;
 }
+
 // front end
 $(document).ready(function() {
   $("#orderPizza").submit(function(event) {
@@ -15,11 +16,16 @@ $(document).ready(function() {
     var crust = $("input:radio[name=crust]:checked").val();
     var cheese = $("input:radio[name=cheese]:checked").val();
     var meats = [];
+    var veggies = [];
     $("input:checkbox[name=meats]:checked").each(function(){
       var checkedMeats = $(this).val();
       meats.push(checkedMeats);
     });
-    var veggies = "green pepper";
+    $("input:checkbox[name=veggies]:checked").each(function(){
+      var checkedVeggies = $(this).val();
+      veggies.push(checkedVeggies);
+    });
+
     var newPizza = new Pizza(crust, cheese, meats, veggies);
 
     $("#checkoutCart").append('<div>' + newPizza.displayOrder() +'</div>');
